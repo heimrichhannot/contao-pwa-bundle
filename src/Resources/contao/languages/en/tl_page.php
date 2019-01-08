5e7f45eacaf9b841412410d9499ab35e38c4df31
Thomas Körner <t.koerner@heimrich-hannot.de>
2019-01-08 15:39:26 +0100

    finished unfinished file translation

--- src/Resources/contao/languages/en/tl_page.php
+++ src/Resources/contao/languages/en/tl_page.php
@@ -8,10 +8,8 @@ $lang = &$GLOBALS['TL_LANG']['tl_page'];
 $lang['addPwa'] = ["Activate Progressive Web App (PWA)","Make current page an progressive web app."];
 $lang['addPwa'][\HeimrichHannot\ContaoPwaBundle\DataContainer\PageContainer::ADD_PWA_NO] = 'No';
 $lang['addPwa'][\HeimrichHannot\ContaoPwaBundle\DataContainer\PageContainer::ADD_PWA_YES] = 'Yes';
-$lang['addPwa'][\HeimrichHannot\ContaoPwaBundle\DataContainer\PageContainer::ADD_PWA_INHERIT] = 'Inherit from parent page';
 
-$lang['pwaConfiguration'] = ["PWA-Konfiguration auswählen","Wählen Sie hier die Konfiguration für die Progressive Web App."];
-$lang['pwaParent'] = ["Eltern-Seite","Wählen Sie hier die Eltern-Seite für die Progressive Web App, von welcher der aktuelle Seitenbaum erben soll. Dies ist sinnvoll bei mehrsprachigen Seiten, welche die gleiche Konfiguration erhalten sollen."];
+$lang['pwaConfiguration'] = ["Choose PWA configuration","Choose your web app configuration"];
 
 /**
  * Legends
