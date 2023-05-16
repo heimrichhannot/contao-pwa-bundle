a930ee2f56b21f9a32f58ebb1350aab71949d926
Thomas Körner <t.koerner@heimrich-hannot.de>
2023-05-16 13:26:11 +0200

    update translations

--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -13,5 +13,7 @@ $lang = &$GLOBALS['TL_LANG']['tl_content'];
 $lang['pwaSubscribeButtonTemplate'][0]   = "Button template";
 $lang['pwaSubscribeButtonTemplate'][1]   = 'Choose the template for the "subscribe notifications" button.';
 $lang['linkTitle']['pwa_install_button'] = 'This text is show instead of the default text "Add to home screen".';
+$lang['text']['pwa_0'] = '"Installation not supported" text';
+$lang['text']['pwa_1'] = 'Add the message that should be displayed when the installation of an pwa is not supported or can be triggered. Leave empty to show no message.';
 $lang['pwaButtonCssClasses'][0]          = 'Button css classes';
 $lang['pwaButtonCssClasses'][1]          = 'Add custom css classes for the button element, "btn btn-primary" for bootstrap 5 for example.';
