d5dd5f015ce9a6df1f4b63cc1fd2711eeaf18804
Thomas Körner <t.koerner@heimrich-hannot.de>
2026-03-05 12:09:32 +0100

    add qa tools

---
+++ rector.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+use Contao\Rector\Set\ContaoLevelSetList;
+use Contao\Rector\Set\ContaoSetList;
+use Rector\Config\RectorConfig;
+use Rector\Php81\Rector\Array_\ArrayToFirstClassCallableRector;
+use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\ValueObject\PhpVersion;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+//        __DIR__ . '/contao',
+
+    ])
+    ->withPhpVersion(PhpVersion::PHP_84)
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+        # In Vorbereitung für PHP 8.4:
+        ExplicitNullableParamTypeRector::class,
+    ])
+
+    ->withImportNames(
+        importShortClasses: false,
+        removeUnusedImports: true
+    )
+    ->withComposerBased(
+        twig: true,
+        doctrine: true,
+        phpunit: true,
+        symfony: true,
+    )
+    ->withSets([
+        LevelSetList::UP_TO_PHP_74,
+        ContaoLevelSetList::UP_TO_CONTAO_49,
+        ContaoSetList::FQCN,
+        ContaoSetList::ANNOTATIONS_TO_ATTRIBUTES,
+    ])
+    ->withSkip([
+        ArrayToFirstClassCallableRector::class,
+    ])
+;
